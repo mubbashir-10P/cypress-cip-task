0c49ad8007403d862d0d8f5927c33a353c928762
mubbashir.shakil <mubbashir.shakil@10pearls.com>
2023-06-17 19:50:56 +0500

    change parallel settings to signle container. also adding tests in admin class

---
+++ cypress/e2e/orangehrm/admin.spec.cy.js
@@ -0,0 +1,8 @@
+describe("Demo test", ()=> {
+    it("Should verify That 2 is equals to 2",() => {
+        expect(2).to.equal(2)
+    })
+    it("Should verify That 3 is equals to 3",() => {
+        expect(3).to.equal(3)
+    })
+})
